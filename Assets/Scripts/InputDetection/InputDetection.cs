bf8abbca590dddfc3f0748f0461590d3c908e070
Mykola Kutsoloshchenko <asdfukmail@gmail.com>
2022-11-20 22:32:26 -0600

    Updated unity to latest version Updated to new Input System

--- Assets/Scripts/InputDetection/InputDetection.cs
+++ Assets/Scripts/InputDetection/InputDetection.cs
@@ -2,58 +2,152 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace OrkWizard
 {
     public class InputDetection : MonoBehaviour
     {
-        private const string _horizontalAxis = "Horizontal";
+        // HorizontalInputs
+        public Vector2 HorizontalInput { get; private set; }
+
+
+        public bool Manual { get; private set; }
+        public bool Walk { get; private set; }
+        public bool JumpBeingPressed { get; private set; }
+        public bool AttackBeingPressed { get; private set; }
+
+        private const string _weaponPressed = "ChangeWeapon";
+        private const string _downPressed = "Down";
+        private const string _jump = "Jump";
+
+        private PlayerInput playerInput;
         protected InputMapper mapper = new InputMapper();
 
-
-        public bool WalkingHeld()
+        private void Awake()
         {
-            return Input.GetKey(mapper.WalkKeyCode);
+            playerInput = GetComponent<PlayerInput>();
         }
 
-        public bool ManualHeld()
+        public void OnHorizontalAxisChange(InputAction.CallbackContext context)
         {
-            return Input.GetKey(mapper.ManualKeyCode);
+            if (!context.canceled)
+            {
+                HorizontalInput = context.ReadValue<Vector2>();
+            }
+            else
+            {
+                HorizontalInput = Vector2.zero;
+            }
         }
 
-        public float GetHorizontalMovement()
+        public void OnWalkingHeld(InputAction.CallbackContext context)
         {
-            return Input.GetAxis(_horizontalAxis);
+            if (context.started || context.performed)
+            {
+                Walk = true;
+            }
+            else
+            {
+                Walk = false;
+            }
+
         }
 
-        public bool JumpHeld()
+        public void OnManualHeld(InputAction.CallbackContext context)
         {
-            return Input.GetKey(mapper.JumpKeyCode);
+            if (context.started || context.performed)
+            {
+                Manual = true;
+            }
+            else
+            {
+                Manual = false;
+            }
+
         }
 
-        internal bool AttackHeld()
+        public void OnJumpHeld(InputAction.CallbackContext context)
         {
-            return Input.GetKey(mapper.AttackKeyCode);
+            if (context.started || context.performed)
+            {
+                JumpBeingPressed = true;
+            }
+            else
+            {
+                JumpBeingPressed = false;
+            }
+        }
+
+        public void OnAttack(InputAction.CallbackContext context)
+        {
+            if (context.started || context.performed)
+            {
+                AttackBeingPressed = true;
+            }
+            else
+            {
+                AttackBeingPressed = false;
+            }
+        }
+
+        public void OnWeaponSwitch(InputAction.CallbackContext context)
+        {
+            if (context.started)
+            {
+                AttackBeingPressed = true;
+            }
+
+        }
+
+        //public bool WalkingHeld()
+        //{
+        //    return Input.GetKey(mapper.WalkKeyCode);
+        //}
+
+        //public bool ManualHeld()
+        //{
+        //    return Input.GetKey(mapper.ManualKeyCode);
+        //}
+
+        //public float GetHorizontalMovement()
+        //{
+        //    return Input.GetAxis(_horizontalAxis);
+        //}
+
+        //public bool JumpHeld()
+        //{
+        //    return Input.GetKey(mapper.JumpKeyCode);
+        //}
+
+        //internal bool AttackHeld()
+        //{
+        //    return Input.GetKey(mapper.AttackKeyCode);
+        //}
+
+        //public bool JumpPressed()
+        //{
+        //    return Input.GetKeyDown(mapper.JumpKeyCode);
+        //}
+
+        public bool SwitchWeaponPressed()
+        {
+            return playerInput.currentActionMap.FindAction(_weaponPressed).WasPressedThisFrame();
         }
 
         public bool JumpPressed()
         {
-            return Input.GetKeyDown(mapper.JumpKeyCode);
-        }
-
-        internal bool SwitchWeaponPressed()
-        {
-            return Input.GetKeyDown(mapper.SwitchWeaponKey);
+            return playerInput.currentActionMap.FindAction(_jump).WasPressedThisFrame();
         }
 
         public bool DownPressed()
         {
-            return Input.GetKeyDown(mapper.Down);
+            return playerInput.currentActionMap.FindAction(_downPressed).WasPressedThisFrame();
         }
 
         public bool RestartPressed()
         {
-            return Input.GetKeyDown(mapper.Restart);
+            return playerInput.currentActionMap.FindAction("Restart").WasPressedThisFrame();
         }
 
     }
