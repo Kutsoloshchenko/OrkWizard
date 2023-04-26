1e82fec0c21e8f722b361593abd36d609b9bf14f
Mykola Kutsoloshchenko <asdfukmail@gmail.com>
2023-04-26 03:05:44 -0500

    Implement a forward ground check for grounded enemy

--- Assets/Scripts/EnemyScripts/Enemy.cs
+++ Assets/Scripts/EnemyScripts/Enemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3eec85c5a992f23625b7a32c3b47006e76d0bbdf363c212227b542aefc1cc70e
-size 5444
+oid sha256:7e71d8b9f6f35c3a2c294f9d64954ab69da00a183837c2b7c11f906d2cdd2a1b
+size 5975
--- Assets/Scripts/EnemyScripts/Movement/GroundEnemyMovement.cs
+++ Assets/Scripts/EnemyScripts/Movement/GroundEnemyMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6c04c5ac831d83ac816e7a0603d3041b92967cd904aa7404d7f645abe322cce
-size 1731
+oid sha256:0a8bbb5fa61a861e66affcbb80aac12e9d1d8b647ccbc43816b21b119ad55f2a
+size 1818
