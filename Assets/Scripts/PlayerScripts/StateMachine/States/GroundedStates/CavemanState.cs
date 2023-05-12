3a6cce2afd705b2586a0d0fff0dc450e61e989a1
Mykola Kutsoloshchenko <asdfukmail@gmail.com>
2023-05-12 11:33:12 -0500

    Implemented Caveman

---
+++ Assets/Scripts/PlayerScripts/StateMachine/States/GroundedStates/CavemanState.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0665cb4bb2d82a4309145baa5db55dddcdc33983381317cc659d530cb05ad729
+size 1534
--- Assets/Scripts/ScriptableObjects/PlayerSO/PlayerSO.cs
+++ Assets/Scripts/ScriptableObjects/PlayerSO/PlayerSO.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f6f9ccbb9f56b097edcc2065ec1c2e3e007d76b308f2911b168f51bf4acfb8d
-size 2374
+oid sha256:87261c7f4655da2e3e5f831748913254e3d8eee969a74be2a314f97685e93c34
+size 2446
