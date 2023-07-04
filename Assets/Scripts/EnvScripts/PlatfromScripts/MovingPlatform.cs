ee53eb9741c76a20b9f1b3bd687732cb7868f646
Mykola Kutsoloshchenko <asdfukmail@gmail.com>
2023-07-04 02:50:42 -0500

    Line endings

--- Assets/Scripts/EnvScripts/PlatfromScripts/MovingPlatform.cs
+++ Assets/Scripts/EnvScripts/PlatfromScripts/MovingPlatform.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e9e611039c40f890c0a81d6a2cfe3538e9084f0a77430aba2984761ce7bd8d3
-size 6584
+oid sha256:5e83f2be3bfa974381487f4bbe3b686d5dcbe4840cf91361202eeaf617a8cd36
+size 6790
