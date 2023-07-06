903279cdf6e1f8a55c5a45ea9526f2fdc136dce7
Mykola Kutsoloshchenko <asdfukmail@gmail.com>
2023-07-06 17:00:11 -0500

    Added a new level, added some design. changed shovit to be invul vs an enemy projectile

--- Assets/Scripts/EnvScripts/PlatfromScripts/PlatformHandler.cs
+++ Assets/Scripts/EnvScripts/PlatfromScripts/PlatformHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9ae0c1c3d2edca021a6ef79a0b18a0bd26878a766ad7b6b99c2190b0eca6dbd
-size 2044
+oid sha256:ae52f8e660389194decdb16fb5e387689a315bd59ee34bd60f76fe3dc09df65b
+size 2144
