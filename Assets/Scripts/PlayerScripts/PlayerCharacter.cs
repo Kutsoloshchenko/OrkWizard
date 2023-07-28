dab1b5b87c2a181adce3f98649884e1f0dffdc93
Mykola Kutsoloshchenko <asdfukmail@gmail.com>
2023-07-28 00:39:01 -0500

    Implemented bouncing platform Implemented spicked paltform

--- Assets/Scripts/PlayerScripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerScripts/PlayerCharacter.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a89906963ea739e3b0206d03d50eadd84cd6c4bcb9561c17065395a8b2b6421a
-size 11409
+oid sha256:de965b58afc8c60e836419393cdab2b1912a17ced2edb42e13ff44b207fbf271
+size 11435
---
+++ Assets/Scripts/ScriptableObjects/EnvSO/PlatformSO/BounceSO.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cf0df70ab0d027993aa864976daf3d33dd992be746d30e7de1458a1df2a7ce9f
+size 740
